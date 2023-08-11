b6a7f73a412329f27df0f0bcc098d54c9be65501
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-11 11:44:18 -0600

    Tablas de Zonas Sur y centro agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LooginComponent } from './components/loogin/loogin.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ZonaPacificoComponent } from './components/zona-pacifico/zona-pacifico.component';
 import { ZonaNorteComponent } from './components/zona-norte/zona-norte.component';
+import { ZonaCentroComponent } from './components/zona-centro/zona-centro.component';
+import { ZonaSuresteComponent } from './components/zona-sureste/zona-sureste.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ZonaNorteComponent } from './components/zona-norte/zona-norte.component
     LooginComponent,
     InicioComponent,
     ZonaPacificoComponent,
-    ZonaNorteComponent
+    ZonaNorteComponent,
+    ZonaCentroComponent,
+    ZonaSuresteComponent
   ],
   imports: [
     BrowserModule,
