56b8ccea415bd8c20a3785e3ba74395dc9271d3e
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-11 11:31:39 -0600

    Tablas zona Noreste agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LooginComponent } from './components/loogin/loogin.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ZonaPacificoComponent } from './components/zona-pacifico/zona-pacifico.component';
+import { ZonaNorteComponent } from './components/zona-norte/zona-norte.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LooginComponent,
     InicioComponent,
-    ZonaPacificoComponent
+    ZonaPacificoComponent,
+    ZonaNorteComponent
   ],
   imports: [
     BrowserModule,
