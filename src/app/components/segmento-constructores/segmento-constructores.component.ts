34d2a787055f078685fbf6299fedbc038e1159d2
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-14 11:38:17 -0600

    segmento constructores agregado

---
+++ src/app/components/segmento-constructores/segmento-constructores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segmento-constructores',
+  templateUrl: './segmento-constructores.component.html',
+  styleUrls: ['./segmento-constructores.component.css']
+})
+export class SegmentoConstructoresComponent {
+
+}
