df804f1cda92a91bb18219d2e1ea32cadfdd49ac
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-14 16:17:42 -0600

    los 4 botones de las regiones del mapa ya funcionan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LooginComponent } from './components/loogin/loogin.component';
+import { ZonaPacificoComponent } from './components/zona-pacifico/zona-pacifico.component';
+import { ZonaNorteComponent } from './components/zona-norte/zona-norte.component';
+import { ZonaSuresteComponent } from './components/zona-sureste/zona-sureste.component';
+import { ZonaCentroComponent } from './components/zona-centro/zona-centro.component';
+import { SegmentoConstructoresComponent } from './components/segmento-constructores/segmento-constructores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'loggin', pathMatch: 'full'},
+  {path:'cerrar-sesion', component: LooginComponent},
+  {path:'inicio', component: InicioComponent},
+  {path:'Zona-pacifico', component: ZonaPacificoComponent},
+  {path:'Zona-noreste', component: ZonaNorteComponent},
+  {path:'Zona-sureste', component: ZonaSuresteComponent},
+  {path:'Zona-centro', component: ZonaCentroComponent},
+  {path:'contructores', component: SegmentoConstructoresComponent}
+  
+
+  
+
+
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
