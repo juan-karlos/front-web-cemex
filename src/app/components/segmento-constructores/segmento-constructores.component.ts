6c22869df63a42425833552940ce77421bf2be3d
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-15 13:23:06 -0600

    Botones de zona ya redireccionan

--- src/app/components/segmento-constructores/segmento-constructores.component.ts
+++ src/app/components/segmento-constructores/segmento-constructores.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-segmento-constructores',
   templateUrl: './segmento-constructores.component.html',
   styleUrls: ['./segmento-constructores.component.css']
 })
-export class SegmentoConstructoresComponent {
-
+export class SegmentoConstructoresComponent implements OnInit{
+  constructor(private router: Router){}
+  ngOnInit() {
+    
+  }
 }
