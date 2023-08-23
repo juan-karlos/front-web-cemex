f5462c7673ae1c3b157edc54b166af84e3a77f1b
PANPAN24 <mizael.valadez.5@gmail.com>
2023-08-23 17:16:00 -0600

    linea constructores pacifico de prueba terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,39 @@ import { TransportePacificoComponent } from './components/transporte-pacifico/tr
 import { TransporteCentroComponent } from './components/transporte-centro/transporte-centro.component';
 import { TransporteSuresteComponent } from './components/transporte-sureste/transporte-sureste.component';
 import { TransporteNoresteComponent } from './components/transporte-noreste/transporte-noreste.component';
+import { GraficasConstructoresCentroComponent } from './components/graficas-constructores-centro/graficas-constructores-centro.component';
+import { GraficasConstructoresNoresteComponent } from './components/graficas-constructores-noreste/graficas-constructores-noreste.component';
+import { GraficasConstructoresPacificoComponent } from './components/graficas-constructores-pacifico/graficas-constructores-pacifico.component';
+import { GraficasConstructoresSuresteComponent } from './components/graficas-constructores-sureste/graficas-constructores-sureste.component';
+import { GraficasConstruramaCentroComponent } from './components/graficas-construrama-centro/graficas-construrama-centro.component';
+import { GraficasConstruramaNoresteComponent } from './components/graficas-construrama-noreste/graficas-construrama-noreste.component';
+import { GraficasConstruramaPacificoComponent } from './components/graficas-construrama-pacifico/graficas-construrama-pacifico.component';
+import { GraficasConstruramaSuresteComponent } from './components/graficas-construrama-sureste/graficas-construrama-sureste.component';
+import { GraficasIndustrialesCentroComponent } from './components/graficas-industriales-centro/graficas-industriales-centro.component';
+import { GraficasIndustrialesPacificoComponent } from './components/graficas-industriales-pacifico/graficas-industriales-pacifico.component';
+import { GraficasIndustrialesNoresteComponent } from './components/graficas-industriales-noreste/graficas-industriales-noreste.component';
+import { GraficasIndustrialesSuresteComponent } from './components/graficas-industriales-sureste/graficas-industriales-sureste.component';
+import { GraficasNoOperativosCentroComponent } from './components/graficas-no-operativos-centro/graficas-no-operativos-centro.component';
+import { GraficasNoOperativosNoresteComponent } from './components/graficas-no-operativos-noreste/graficas-no-operativos-noreste.component';
+import { GraficasNoOperativosPacificoComponent } from './components/graficas-no-operativos-pacifico/graficas-no-operativos-pacifico.component';
+import { GraficasNoOperativosSuresteComponent } from './components/graficas-no-operativos-sureste/graficas-no-operativos-sureste.component';
+import { GraficasOperacionesCentroComponent } from './components/graficas-operaciones-centro/graficas-operaciones-centro.component';
+import { GraficasOperacionesNoresteComponent } from './components/graficas-operaciones-noreste/graficas-operaciones-noreste.component';
+import { GraficasOperacionesPacificoComponent } from './components/graficas-operaciones-pacifico/graficas-operaciones-pacifico.component';
+import { GraficasOperacionesSuresteComponent } from './components/graficas-operaciones-sureste/graficas-operaciones-sureste.component';
+import { GraficasPromexmaCentroComponent } from './components/graficas-promexma-centro/graficas-promexma-centro.component';
+import { GraficasPromexmaNoresteComponent } from './components/graficas-promexma-noreste/graficas-promexma-noreste.component';
+import { GraficasPromexmaPacificoComponent } from './components/graficas-promexma-pacifico/graficas-promexma-pacifico.component';
+import { GraficasPromexmaSuresteComponent } from './components/graficas-promexma-sureste/graficas-promexma-sureste.component';
+import { GraficasSuministroCentroComponent } from './components/graficas-suministro-centro/graficas-suministro-centro.component';
+import { GraficasSuministroNoresteComponent } from './components/graficas-suministro-noreste/graficas-suministro-noreste.component';
+import { GraficasSuministroPacificoComponent } from './components/graficas-suministro-pacifico/graficas-suministro-pacifico.component';
+import { GraficasSuministroSuresteComponent } from './components/graficas-suministro-sureste/graficas-suministro-sureste.component';
+import { GraficasTransporteCentroComponent } from './components/graficas-transporte-centro/graficas-transporte-centro.component';
+import { GraficasTransporteNoresteComponent } from './components/graficas-transporte-noreste/graficas-transporte-noreste.component';
+import { GraficasTransportePacificoComponent } from './components/graficas-transporte-pacifico/graficas-transporte-pacifico.component';
+import { GraficasTransporteSuresteComponent } from './components/graficas-transporte-sureste/graficas-transporte-sureste.component';
+import { NivelRiesgoConstructoresPacificoComponent } from './components/nivel-riesgo-constructores-pacifico/nivel-riesgo-constructores-pacifico.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'loggin', pathMatch: 'full'},
@@ -91,6 +124,40 @@ const routes: Routes = [
   {path:'transporte-centro', component: TransporteCentroComponent },
   {path:'transporte-sureste', component: TransporteSuresteComponent },
   {path:'transporte-noreste', component: TransporteNoresteComponent},
+  {path:'graficas-constructores-centro', component: GraficasConstructoresCentroComponent},
+  {path:'graficas-constructores-noreste', component: GraficasConstructoresNoresteComponent},
+  {path:'graficas-constructores-pacifico', component: GraficasConstructoresPacificoComponent},
+  {path:'graficas-constructores-sureste', component: GraficasConstructoresSuresteComponent},
+  {path:'graficas-construrama-centro', component: GraficasConstruramaCentroComponent},
+  {path:'graficas-construrama-noreste', component: GraficasConstruramaNoresteComponent},
+  {path:'graficas-construrama-pacifico', component: GraficasConstruramaPacificoComponent},
+  {path:'graficas-construrama-sureste', component: GraficasConstruramaSuresteComponent},
+  {path:'graficas-industriales-centro', component: GraficasIndustrialesCentroComponent},
+  {path:'graficas-industriales-pacifico', component: GraficasIndustrialesPacificoComponent},
+  {path:'graficas-industriales-noreste', component: GraficasIndustrialesNoresteComponent},
+  {path:'graficas-industriales-sureste', component: GraficasIndustrialesSuresteComponent},
+  {path:'graficas-no-operativos-centro', component: GraficasNoOperativosCentroComponent},
+  {path:'graficas-no-operativos-noreste', component: GraficasNoOperativosNoresteComponent},
+  {path:'graficas-no-operativos-pacifico', component: GraficasNoOperativosPacificoComponent},
+  {path:'graficas-no-operativos-sureste', component: GraficasNoOperativosSuresteComponent},
+  {path:'graficas-operaciones-centro', component: GraficasOperacionesCentroComponent},
+  {path:'graficas-operaciones-noreste', component: GraficasOperacionesNoresteComponent},
+  {path:'graficas-operaciones-pacifico', component: GraficasOperacionesPacificoComponent},
+  {path:'graficas-operaciones-sureste', component: GraficasOperacionesSuresteComponent},
+  {path:'graficas-promexma-centro', component: GraficasPromexmaCentroComponent},
+  {path:'graficas-promexma-noreste', component: GraficasPromexmaNoresteComponent},
+  {path:'graficas-promexma-pacifico', component: GraficasPromexmaPacificoComponent},
+  {path:'graficas-promexma-sureste', component: GraficasPromexmaSuresteComponent},
+  {path:'graficas-suministro-centro', component: GraficasSuministroCentroComponent},
+  {path:'graficas-suministro-noreste', component: GraficasSuministroNoresteComponent},
+  {path:'graficas-suministro-pacifico', component: GraficasSuministroPacificoComponent},
+  {path:'graficas-suministro-sureste', component: GraficasSuministroSuresteComponent},
+  {path:'graficas-transporte-centro', component: GraficasTransporteCentroComponent},
+  {path:'graficas-transporte-noreste', component: GraficasTransporteNoresteComponent},
+  {path:'graficas-transporte-pacifico', component: GraficasTransportePacificoComponent},
+  {path:'graficas-transporte-sureste', component: GraficasTransporteSuresteComponent},
+  {path:'nivel-riesgo-constructores-pacifico', component: NivelRiesgoConstructoresPacificoComponent},
+ 
 
 ];
 
