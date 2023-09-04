8a5801159da4ad3576baf19fa634fe2b26d30e98
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-04 14:55:40 -0600

    Se han agregado los modelos para las tablas Version 1

--- src/app/components/models/tablas.ts
+++ src/app/components/models/tablas.ts
@@ -1,5 +1,42 @@
 export interface usuario{
+  id_usuario: number
   correo:String,
   nombre:String,
   contraseña:String
 }
+export interface planta{
+  id_planta: number,
+  nombre:String,
+  segmento: String,
+  zona: String,
+  estado: String,
+  procentaje_cumplimiento: number,
+  fija: Boolean
+}
+
+export interface permiso{
+  id_requerimiento: number,
+  nombre_requerimiento: String,
+  peso: number,
+  impacto: String
+}
+
+export interface registro{
+id_registro: number,
+id_planta: number,
+id_requerimiento: number,
+fecha_inicio: Date,
+fecha_vencimiento: Date,
+observaciones: String,
+estatus: String,
+validez_unica: boolean,
+url:String
+}
+export interface historial{
+id_historial: number,
+segmento: String,
+zona: String,
+procentaje_cimplimiento: number,
+fecha: Date
+}
+
