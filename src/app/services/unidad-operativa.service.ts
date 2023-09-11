2221e7fc2e3476d0519720bc5b4eb7d90e28af6c
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-11 10:22:51 -0600

    Se genero el servicio de plantas pero esta vacio

---
+++ src/app/services/unidad-operativa.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {planta} from '../components/models/tablas'
+@Injectable({
+  providedIn: 'root'
+})
+export class UnidadOperativaService {
+URL_API = ''
+  constructor() { }
+} 
