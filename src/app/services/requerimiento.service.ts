2ed5717f7a273247b1e415baf26f09ba060bb7c9
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-11 15:50:47 -0600

    servicio requerimiento creado, falta modificar la url de peticiones

---
+++ src/app/services/requerimiento.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {permiso} from '../components/models/tablas'
+@Injectable({
+  providedIn: 'root'
+})
+export class RequerimientoService {
+  URL_API = 'http://192.168.100.62:3500/api/requerimiento/'
+  Permiso:permiso[]=[];
+  
+  PermisoSelect :permiso={
+    id_requerimiento: 0,
+    nombre_requerimiento: '',
+    peso: 0,
+    impacto:'',
+    siglas:''
+    }
+    constructor(private http:HttpClient) { }
+
+    obtenerpermiso(){
+      return this.http.get<permiso[]>(this.URL_API);
+    }
+}
