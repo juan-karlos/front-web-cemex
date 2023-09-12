43717804ac876be53e0a0a37836729ccf76bc03b
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-12 11:34:25 -0600

    Se comenzó a desarrollar la interfaz de ingresar nuevo permiso

---
+++ src/app/components/agregar-registro/agregar-registro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-registro',
+  templateUrl: './agregar-registro.component.html',
+  styleUrls: ['./agregar-registro.component.css']
+})
+export class AgregarRegistroComponent {
+
+}
