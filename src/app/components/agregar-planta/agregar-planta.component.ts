65e318dde1baf536afca040fa55212804bc41c77
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-18 13:44:48 -0600

    Se agrego el metodo de mostrar alerta pero falta relacionarlo al insert

--- src/app/components/agregar-planta/agregar-planta.component.ts
+++ src/app/components/agregar-planta/agregar-planta.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UnidadOperativaService } from 'src/app/services/unidad-operativa.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-planta',
@@ -22,5 +23,44 @@ export class AgregarPlantaComponent {
       }
     )
   }
+  
+ validacion(form:NgForm){
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger',
+      
+      
+    },
+    buttonsStyling: true
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: '¿Los datos son correctos?',
+    text: "Asegúrate de que los datos sean corectos",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, agregar',
+    cancelButtonText: 'Volver a revisar',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Agregado',
+        'La planta fue agregada',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'Revisa que los datos esten correctos',
+        'error'
+      )
+    }
+  })
+ }
 
 }
