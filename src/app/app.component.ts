1ab038af2397296ac8589aa1a04f78fd6452c843
PANPAN24 <mizael.valadez.5@gmail.com>
2023-09-25 10:58:19 -0600

    Se elimino el ngOninit de app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(){}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class AppComponent{
+  
   title = 'front-cemex';
 }
