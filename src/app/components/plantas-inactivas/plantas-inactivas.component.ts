e731ede34dfddf2a63f2816d3fd76864b5a4ef8d
PANPAN24 <mizael.valadez.5@gmail.com>
2023-10-03 14:25:22 -0600

    El componente ya muetra datos pero aun no filtran las plantas activas e inactivas

--- src/app/components/plantas-inactivas/plantas-inactivas.component.ts
+++ src/app/components/plantas-inactivas/plantas-inactivas.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {UnidadOperativaService} from 'src/app/services/unidad-operativa.service';
+import { FormsModule } from '@angular/forms'; 
+
 
 @Component({
   selector: 'app-plantas-inactivas',
   templateUrl: './plantas-inactivas.component.html',
   styleUrls: ['./plantas-inactivas.component.css']
 })
-export class PlantasInactivasComponent {
+export class PlantasInactivasComponent implements OnInit{
+
+  constructor(public unidad:UnidadOperativaService){}
+  filterPost ='';
+  
+  ngOnInit(): void {
+    this.obtenerplantas();
+    
+  }
+
+
+  obtenerplantas(){
+    this.unidad.obtenerplanta().subscribe(
+      res=>this.unidad.Plantas=res,
+      err=>console.error(err),
+      
+    )
+    
+  }
 
 }
