32b3f29dbfe5653a8d98d68446e4b2c350da1294
PANPAN24 <mizael.valadez.5@gmail.com>
2023-11-27 16:37:41 -0600

    la lista de actualizar registro ya fue creada y trae datos al form, pero aun no se arregla en enlace de la url

--- src/app/components/actualizar-registro/actualizar-registro.component.ts
+++ src/app/components/actualizar-registro/actualizar-registro.component.ts
@@ -1,93 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { RegistrosService } from 'src/app/services/registros.service';
-import { NgForm } from '@angular/forms';
-import * as moment from 'moment';
-import { MatCheckboxChange } from '@angular/material/checkbox';
 import Swal from 'sweetalert2';
-import { RequerimientoService } from 'src/app/services/requerimiento.service';
-
-moment.locale('es');
+import { RegistrosService } from 'src/app/services/registros.service'; 
+import { FormBuilder, NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-agregar-registro',
-  templateUrl: './agregar-registro.component.html',
-  styleUrls: ['./agregar-registro.component.css']
+  selector: 'app-actualizar-registro',
+  templateUrl: './actualizar-registro.component.html',
+  styleUrls: ['./actualizar-registro.component.css']
 })
-
-export class AgregarRegistroComponent implements OnInit{
+export class ActualizarRegistroComponent implements OnInit {
+  
+  itemId!: number;
+  datos: any;
+  FormRegistro: any;
   arregloRequerimientos: string[] = [];
-
   public fechaHabilitada = true;
   public checkboxActivado = false;
-
-
-  //fechainicio = moment(this.datepicker.value);
-
   mostrar: boolean = false;
- // fechainicio = moment(this,datepicker.value).format(this.formato);
   selectedFile: File | null = null;
   fecha_inicio: string | null = null;
   fecha_vencimiento: string | null = null;
   validez_unica:boolean=false ;
   estatus:string='';
+  url:string='';
   observaciones:string="";
   nombre_requerimiento:string="";
   nombre_planta:string="";
+  id_registro!: number
 
-  constructor(private http: HttpClient,public permiso:RequerimientoService, public Registros:RegistrosService) {
-
+  constructor(private http: HttpClient, public servisregistro:RegistrosService,private FB: FormBuilder,
+    private route: ActivatedRoute){
+    
   }
   ngOnInit(): void {
-   this.obtenerpermisos();
-
+    this.route.params.subscribe(params => {
+      
+      this.itemId = params['id_registro'];
+      console.log('esto es lo que esta mandando:', this.itemId);
+      this.servisregistro.obtenerRegistroPorId(this.itemId).subscribe(objeto => {
+        this.datos = objeto;
+        this.id_registro = this.datos[0].id_registro;
+        this.nombre_planta = this.datos[0].nombre_planta;
+        this.nombre_requerimiento = this.datos[0].nombre_requerimiento;
+        this.fecha_inicio = this.datos[0].fecha_inicio;
+        this.fecha_vencimiento = this.datos[0].fecha_vencimiento;
+        this.observaciones = this.datos[0].observaciones;
+        this.estatus = this.datos[0].estatus;
+        this.validez_unica = this.datos[0].validez_unica;
+        this.url = this.datos[0].url;
+        console.log(objeto);
+      });
+  })
   }
-
-  filterPost ='';
-  num: number = 0;
-
-  obtenerpermisos(){
-    this.permiso.obtenerpermiso().subscribe(
-      (datos) => {
-        this.permiso.Permiso = datos;
-        this.num = datos.length;
-        this.arregloRequerimientos = datos.map((item: any) => item.nombre_requerimiento);
-
-        return datos;
-      },
-      (err) => console.error(err)
-
-    )
-  }
-
-  toggleDatepickers(event: MatCheckboxChange) {
-    this.checkboxActivado = event.checked;
-    this.fechaHabilitada = !event.checked;
-    this.fecha_inicio = null; //puedes reiniciar las fechas cuando se deshabilitan.
-    this.fecha_vencimiento = null;
-  }
-  enviardat(){
-    const fechas={
-     fecha_inicio: this.fecha_inicio,
-     fecha_vencimiento: this.fecha_vencimiento
-
-    }
-  }
-
-  onFileSelected(event: any) {
-    console.log("archivo seleccionado");
-    this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile);
-  }
-
-  mostrarFechas(){
-    if(this.mostrar){
-      this.mostrar = false;
-    }
-    else{
-       this.mostrar = true;
-    }
-  }
-
 
 
   onSubmit() {
@@ -99,7 +65,7 @@ export class AgregarRegistroComponent implements OnInit{
       buttonsStyling: true
     });
     const fecha1 = moment(this.fecha_inicio);
-    const fecha2=moment(this.fecha_vencimiento);
+    const fecha2= moment(this.fecha_vencimiento);
 
     const fechaAcomodada = fecha1.format('YYYY/MM/DD');
 
@@ -180,6 +146,8 @@ export class AgregarRegistroComponent implements OnInit{
 
 
 
+
+  
   validacion() {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
@@ -220,8 +188,6 @@ export class AgregarRegistroComponent implements OnInit{
           }
         });
     }
-  }
-
-
-
-
+  
+   
+}
