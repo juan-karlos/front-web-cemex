0b3af2b4caed708ea58b96bd761e43062e0295f2
PANPAN24 <mizael.valadez.5@gmail.com>
2023-11-30 09:59:25 -0600

    Se arego funcionalidad al servicio de la logica

--- src/app/services/logica.service.ts
+++ src/app/services/logica.service.ts
@@ -8,7 +8,26 @@ export class LogicaService {
  URL_API='http://localhost:3200/api/logica';
 
 
-
-
   constructor(private http:HttpClient) { }
+
+  
+  getPesoTotal(nombre: string){
+    return this.http.post<any>(`${this.URL_API}/pesoTotal`, { nombre });
+  }
+
+  getPesoParcial(estatu: string, nombre: string){
+    return this.http.post<any>(`${this.URL_API}/pesoParcial`, { estatu, nombre });
+  }
+
+  getPesoEnPorcentajeEstatus(nomPlanta: string, estatus: string){
+    return this.http.post<any>(`${this.URL_API}/pesoEnPorcentajeEstatus`, { nomPlanta, estatus });
+  }
+
+  getTotalPlantas(zona: string, segmento: string){
+    return this.http.post<any>(`${this.URL_API}/totalPlantas`, { zona, segmento });
+  }
+
+  getSumTotalZonaSegmento(zona: string, segmento: string){
+    return this.http.post<any>(`${this.URL_API}/sumTotalZonaSegmento`, { zona, segmento });
+  }
 }
