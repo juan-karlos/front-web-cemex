5ac104cceb271f75b7617c17a53b858381583523
PANPAN24 <mizael.valadez.5@gmail.com>
2023-12-13 15:15:51 -0600

    Se actualizaron las verificaciones para la inserción y actualización de registros

--- src/app/components/agregar-registro/agregar-registro.component.ts
+++ src/app/components/agregar-registro/agregar-registro.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { RegistrosService } from 'src/app/services/registros.service';
-import { NgForm } from '@angular/forms';
-import * as moment from 'moment';
-import { MatCheckboxChange } from '@angular/material/checkbox';
-import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
 import { RequerimientoService } from 'src/app/services/requerimiento.service';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { startWith, switchMap } from 'rxjs/operators';
+import * as moment from 'moment';
+import Swal from 'sweetalert2';
 import { UnidadOperativaService } from 'src/app/services/unidad-operativa.service';
 
 moment.locale('es');
+
 @Component({
   selector: 'app-agregar-registro',
   templateUrl: './agregar-registro.component.html',
   styleUrls: ['./agregar-registro.component.css']
 })
-
-export class AgregarRegistroComponent implements OnInit{
-
-
+export class AgregarRegistroComponent implements OnInit {
 
   arregloRequerimientos: string[] = [];
   public fechaHabilitada = true;
@@ -28,29 +23,27 @@ export class AgregarRegistroComponent implements OnInit{
   selectedFile: File | null = null;
   fecha_inicio: string | null = null;
   fecha_vencimiento: string | null = null;
-  validez_unica:boolean=false ;
-  estatus:string='';
-  observaciones:string="";
-  nombre_requerimiento:string="";
-  nombre_planta:string="";
+  validez_unica: boolean = false;
+  estatus: string = '';
+  observaciones: string = "";
+  nombre_requerimiento: string = "";
+  nombre_planta: string = "";
 
-form: FormGroup = new FormGroup({
-    nombre_planta: new FormControl(''), 
+  form: FormGroup = new FormGroup({
+    nombre_planta: new FormControl(''),
   });
 
   nombrePlantaControl = new FormControl();
   sugerenciasNombresPlantas: string[] = [];
 
- 
+  fechaHoy: Date = new Date();
 
   constructor(
     private http: HttpClient,
-    public permiso:RequerimientoService, 
-    public Registros:RegistrosService,
+    public permiso: RequerimientoService,
     private formBuilder: FormBuilder,
     private unidadOperativaService: UnidadOperativaService
   ) {
-    // Configura el autocompletado
     this.nombrePlantaControl.valueChanges
       .pipe(
         startWith(''),
@@ -60,66 +53,42 @@ form: FormGroup = new FormGroup({
         this.sugerenciasNombresPlantas = sugerencias;
       });
   }
-  ngOnInit(): void {
-   this.obtenerpermisos();
 
+  ngOnInit(): void {
+    this.obtenerpermisos();
   }
 
-  filterPost ='';
-  num: number = 0;
-
-  obtenerpermisos(){
+  obtenerpermisos() {
     this.permiso.obtenerpermiso().subscribe(
       (datos) => {
         this.permiso.Permiso = datos;
-        this.num = datos.length;
         this.arregloRequerimientos = datos.map((item: any) => item.nombre_requerimiento);
-
-        return datos;
       },
       (err) => console.error(err)
-
-    )
+    );
   }
 
-  toggleDatepickers(event: MatCheckboxChange) {
+  toggleDatepickers(event: any) {
     this.checkboxActivado = event.checked;
     this.fechaHabilitada = !event.checked;
-    this.fecha_inicio = null; //puedes reiniciar las fechas cuando se deshabilitan.
+    this.fecha_inicio = null;
     this.fecha_vencimiento = null;
-  }
-  enviardat(){
-
-    const fechas={
-     fecha_inicio: this.fecha_inicio,
-     fecha_vencimiento: this.fecha_vencimiento
-    }
   }
 
   onFileSelected(event: any) {
-    console.log("archivo seleccionado");
     this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile);
   }
 
-  mostrarFechas(){
-    if(this.mostrar){
-      this.mostrar = false;
-    }
-    else{
-       this.mostrar = true;
-    }
+  mostrarFechas() {
+    this.mostrar = !this.mostrar;
   }
 
-  actualizar(){
-
-    const nombre_planta=this.nombre_planta;
+  actualizar() {
+    const nombre_planta = this.nombre_planta;
     const formData = new FormData();
-    formData.append('nombre_planta',nombre_planta)
-    this.http.post('http://localhost:3200/api/requerimiento/porcentajeAct',formData)
+    formData.append('nombre_planta', nombre_planta);
+    this.http.post('http://localhost:3200/api/requerimiento/porcentajeAct', formData);
   }
-
-
 
   onSubmit() {
     const swalWithBootstrapButtons = Swal.mixin({
@@ -129,44 +98,33 @@ form: FormGroup = new FormGroup({
       },
       buttonsStyling: true
     });
+
     const fecha1 = moment(this.fecha_inicio);
-    const fecha2=moment(this.fecha_vencimiento);
+    const fecha2 = moment(this.fecha_vencimiento);
 
     const fechaAcomodada = fecha1.format('YYYY/MM/DD');
-
-    const fechaAcomodada2= fecha2.format('YYYY/MM/DD');
+    const fechaAcomodada2 = fecha2.format('YYYY/MM/DD');
     const validez_unica = this.validez_unica ? 'true' : 'false';
-    const estatus=this.estatus;
-    const observaciones=this.observaciones;
-    const nombre_planta=this.nombre_planta;
-    const nombre_requerimiento= this.nombre_requerimiento
+    const estatus = this.estatus;
+    const observaciones = this.observaciones;
+    const nombre_planta = this.nombre_planta;
+    const nombre_requerimiento = this.nombre_requerimiento;
 
     const formData = new FormData();
 
     if (this.selectedFile) {
-
       formData.append('pdfFile', this.selectedFile);
       formData.append('fechaAcomodada', fechaAcomodada);
       formData.append('fechaAcomodada2', fechaAcomodada2);
       formData.append('validez_unica', validez_unica);
-      formData.append('estatus',estatus);
-      formData.append('observaciones',observaciones);
-      formData.append('nombre_planta',nombre_planta);
-      formData.append('nombre_requerimiento',nombre_requerimiento);
-      console.log('Contenido de formData:');
-      formData.forEach((value, key) => {
-      console.log(key, value);
-     });
-      this.http.post('http://localhost:3200/api/regi/pdf',formData).subscribe(
+      formData.append('estatus', estatus);
+      formData.append('observaciones', observaciones);
+      formData.append('nombre_planta', nombre_planta);
+      formData.append('nombre_requerimiento', nombre_requerimiento);
 
-
+      this.http.post('http://localhost:3200/api/regi/pdf', formData).subscribe(
         (response: any) => {
-
-
-      this.actualizar();
-          // `response` puede contener la URL del PDF en el servidor
-          console.log('URL del PDF en el servidor:', response);
-
+          this.actualizar();
           swalWithBootstrapButtons.fire(
             'Agregado',
             'El registro fue agregado ',
@@ -181,25 +139,18 @@ form: FormGroup = new FormGroup({
           );
         }
       );
-    }else {
+    } else {
       formData.append('fechaAcomodada', fechaAcomodada);
       formData.append('fechaAcomodada2', fechaAcomodada2);
       formData.append('validez_unica', validez_unica);
-      formData.append('estatus',estatus);
-      formData.append('observaciones',observaciones);
-      formData.append('nombre_planta',nombre_planta);
-      formData.append('nombre_requerimiento',nombre_requerimiento);
-      console.log('Contenido de formData:');
+      formData.append('estatus', estatus);
+      formData.append('observaciones', observaciones);
+      formData.append('nombre_planta', nombre_planta);
+      formData.append('nombre_requerimiento', nombre_requerimiento);
 
-      formData.forEach((value, key) => {
-      console.log(key, value);
-  });
-      this.http.post('http://localhost:3200/api/regi/pdf',formData).subscribe(
+      this.http.post('http://localhost:3200/api/regi/pdf', formData).subscribe(
         (response: any) => {
-          // `response` puede contener la URL del PDF en el servidor
-          console.log('URL del PDF en el servidor:', response);
-
-      this.actualizar();
+          this.actualizar();
           swalWithBootstrapButtons.fire(
             'Agregado',
             'El registro fue agregado ',
@@ -213,63 +164,46 @@ form: FormGroup = new FormGroup({
             'error'
           );
         }
-      )
+      );
     }
-
   }
 
-
-
-
-  ejecutar(){
+  ejecutar() {
     this.onSubmit();
     this.actualizar();
   }
 
-
-
   validacion() {
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger',
-      },
-      buttonsStyling: true
-    });
-
-
-      // Muestra un mensaje de error si el formulario es inválido o algún campo está vacío
-      swalWithBootstrapButtons.fire(
-        'Error',
-        'Por favor, completa todos los campos antes de agregar.',
-        'error'
-      );
-
-      // Muestra la confirmación si el formulario es válido y los campos están llenos
-      swalWithBootstrapButtons
-        .fire({
-          title: '¿Los datos son correctos?',
-          text: 'Asegúrate de que los datos sean correctos',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Si, agregar',
-          cancelButtonText: 'Cancelar',
-          reverseButtons: true
-        })
-        .then((result) => {
-          if (result.isConfirmed) {
-            this.ejecutar();
-          } else if (result.dismiss === Swal.DismissReason.cancel) {
-            swalWithBootstrapButtons.fire(
-              'Cancelado',
-              'El registro no fue agregado',
-              'error'
-            );
-          }
-        });
+    if (!this.nombre_planta || !this.nombre_requerimiento || !this.estatus) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Por favor, completa todos los campos antes de agregar.',
+      });
+      return;
     }
+  
+    if (!this.validez_unica) {
+      if (!this.fecha_inicio) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, elige una fecha de inicio válida (a partir de hoy).',
+        });
+        return;
+      }
+  
+      if (!this.fecha_vencimiento || moment(this.fecha_vencimiento).isBefore(moment().add(1, 'days'))) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, elige una fecha de vencimiento válida (a partir de mañana).',
+        });
+        return;
+      }
+    }
+  
+    this.ejecutar();
   }
-
-
-
-
+  
+}
